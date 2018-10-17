a5a77c1e9eec390143a30b3d0405fbbaa1681c39
Akshay Maduvinakodi Shashidhar <akshay.ms@exilant.com>
2018-10-17 13:56:15 +0530

    akshay

--- src/main/java/com/qutap/dash/ReactiveWebApplication.java
+++ src/main/java/com/qutap/dash/ReactiveWebApplication.java
@@ -22,6 +22,8 @@ public class ReactiveWebApplication {
 	public Docket api(){
 	return new Docket(DocumentationType.SWAGGER_2)
 								.select().apis(RequestHandlerSelectors.any())
-								.paths(PathSelectors.any()).build();
+								.paths(PathSelectors
+								.any())
+								.build();
 	}
 }
